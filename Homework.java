7883342f286a5eef0a66818811e0dd44a95d8710
Daniel <128633759+S4veZone@users.noreply.github.com>
2023-04-06 12:47:39 +0200

    Basic Java Homework 06.04.2023
    1 уровень сложности: Создайте класс, стартовую точку, где через Scanner вы будете получать целое число и трансформировать его в двоичную систему (выводить на экран)

---
+++ Homework.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter pls Decimal number: ");
+        int n = sc.nextInt();
+        System.out.println("Decimal number: " +n);
+        System.out.println();
+
+        System.out.println("short way 10->2 = " + Integer.toBinaryString(n));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
