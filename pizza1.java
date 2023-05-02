f181ded46a1f0d71212b85677e38cdedba0c7742
Daniel <128633759+S4veZone@users.noreply.github.com>
2023-05-02 15:00:24 +0200

    2023.04.19

--- pizza1.java
+++ pizza1.java
@@ -1,9 +1,9 @@
 package Homework;
 
 public class pizza1 {
-    public double radius1; // radius 1 pizza
-    public double calorii; // calorii
-    public double radius2; // radius 2 pizza
+    private double radius1; // radius 1 pizza
+    private double calorii; // calorii
+    private double radius2; // radius 2 pizza
 
     public pizza1(double radius1, double radius2 , double calorii){
 
@@ -14,7 +14,9 @@ public class pizza1 {
     }
     public void print(){
         System.out.println("radius1: " + radius1 + "; radius2: " + radius2 + "; calorii: " + calorii);
-        System.out.println("raznica v caloriah mejdu 2 pizzami: " +(((Math.PI * (radius1 * radius1)) - (Math.PI * (radius2 * radius2))/calorii)));
+        System.out.println("raznica v caloriah mejdu 2 pizzami: " +
+                (((Math.PI * ((radius1/2) * (radius1/2))) - (Math.PI * ((radius2/2) * (radius2/2))))*calorii));
+
     }
 
 
--- pizzaResult.java
+++ pizzaResult.java
@@ -2,7 +2,7 @@ package Homework;
 
 public class pizzaResult {
     public static void main(String[] args) {
-        pizza1 pizzaResult = new pizza1(14,12,40);
+        pizza1 pizzaResult = new pizza1(28,24,40);
         pizzaResult.print();
     }
 }
