aff898c1c07513d6fa60e912bbc5267e3995e214
Daniel <128633759+S4veZone@users.noreply.github.com>
2023-05-02 15:04:24 +0200

    2023.04.19 Java SORRY !

---
+++ pizzaResult.java
@@ -0,0 +1,8 @@
+package Homework;
+
+public class pizzaResult {
+    public static void main(String[] args) {
+        pizza1 pizzaResult = new pizza1(28,24,40);
+        pizzaResult.print();
+    }
+}
